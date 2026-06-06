1c4e80f07380bd5670615160a6700077537b35f4
Sanju <savi@arch.localdomain>
2026-06-06 10:37:21 +0530

    Add card animation

--- script.js
+++ script.js
@@ -5,32 +5,79 @@ let flag = 1;
 let textTimeout;
 const ball = document.querySelector(".ball");
 
-menu_p.addEventListener("click",()=>{
-    if (flag){
-        over_menu.style.top = "-57%";
-        menu_p.textContent = "CLOSE"
-        // over_menu.style.width = "220%";
-        // over_menu.style.height = "205%";
-        over_menu.style.left = "-60%";
-        over_menu.style.transform = "rotate(25deg) translateY(200px)";
-        textTimeout = setTimeout(() => {
-            over_menu.classList.add("text-animate");
-        }, 1500);
-        flag = 0;
-    }
-    else{
-        over_menu.style.top = "-205%";
-        menu_p.textContent = "MENU"
-        over_menu.style.left = "0%";
-        over_menu.style.transform = "rotate(0deg)";
-        over_menu.style.width = "220%";
-        over_menu.style.height = "205%";
-        clearTimeout(textTimeout);
-        over_menu.classList.remove("text-animate");
+menu_p.addEventListener("click", () => {
+  if (flag) {
+    over_menu.style.top = "-57%";
+    menu_p.textContent = "CLOSE";
+    // over_menu.style.width = "220%";
+    // over_menu.style.height = "205%";
+    over_menu.style.left = "-60%";
+    over_menu.style.transform = "rotate(25deg) translateY(200px)";
+    textTimeout = setTimeout(() => {
+      over_menu.classList.add("text-animate");
+    }, 1500);
+    flag = 0;
+  } else {
+    over_menu.style.top = "-205%";
+    menu_p.textContent = "MENU";
+    over_menu.style.left = "0%";
+    over_menu.style.transform = "rotate(0deg)";
+    over_menu.style.width = "220%";
+    over_menu.style.height = "205%";
+    clearTimeout(textTimeout);
+    over_menu.classList.remove("text-animate");
 
-        flag = 1;
-    }
+    flag = 1;
+  }
 });
+
+const images = document.querySelectorAll(".home_intro_image");
+
+const detail = document.querySelector(".intro-first");
+const playground = document.querySelector(".intro-second");
+
+let interval;
+
+function showImage(word) {
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
+  const card = document.createElement("img");
+  card.src = randomImage.src;
+  card.classList.add("popup-card");
+
+  const wordPosition = word.getBoundingClientRect();
+
+  card.style.left = wordPosition.left + Math.random() * 200 - 100 + "px";
+  card.style.top = wordPosition.top + Math.random() * 150 - 75 + "px";
+
+  document.body.appendChild(card);
+
+  setTimeout(() => {
+    card.remove();
+  }, 1000);
+}
+
+function startPop(word) {
+  interval = setInterval(() => {
+    showImage(word);
+  }, 250);
+}
+
+function stopPop() {
+  clearInterval(interval);
+}
+
+detail.addEventListener("mouseenter", () => {
+  startPop(detail);
+});
+
+detail.addEventListener("mouseleave", stopPop);
+
+playground.addEventListener("mouseenter", () => {
+  startPop(playground);
+});
+
+playground.addEventListener("mouseleave", stopPop);
 
 let mouseX = 0;
 let mouseY = 0;
@@ -38,62 +85,53 @@ let mouseY = 0;
 let currentX = 0;
 let currentY = 0;
 
-document.addEventListener("mousemove", (e)=>{
-
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
 });
 
-function animate(){
+function animate() {
+  currentX += (mouseX - currentX) * 0.08;
+  currentY += (mouseY - currentY) * 0.08;
 
-    currentX += (mouseX - currentX) * 0.08;
-    currentY += (mouseY - currentY) * 0.08;
+  ball.style.left = currentX + "px";
+  ball.style.top = currentY + "px";
 
-    ball.style.left = currentX + "px";
-    ball.style.top = currentY + "px";
-
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 
 animate();
 
 const boxes = document.querySelectorAll(".boxes");
 
-boxes.forEach((box)=>{
+boxes.forEach((box) => {
+  const image = box.querySelector("img");
 
-    const image = box.querySelector("img");
+  box.addEventListener("mouseenter", () => {
+    image.style.filter = "blur(8px)";
+    box.style.height = "940px";
+    box.style.width = "895px";
+    ball.style.width = "125px";
+    ball.style.height = "50px";
+    ball.style.borderRadius = "10px";
+    ball.style.position = "fixed";
+    ball.style.transform = "rotate(-20deg)";
+    ball.style.color = "white";
+    h_boxes.style.visibility = "visible";
+    // h_boxes.style.height = "500px";
+  });
 
-    box.addEventListener("mouseenter", ()=>{
-
-        image.style.filter = "blur(8px)";
-        box.style.height = "940px";
-        box.style.width = "895px";
-        ball.style.width = "125px";
-        ball.style.height = "50px";
-        ball.style.borderRadius = "10px";
-        ball.style.position = "fixed";
-        ball.style.transform = "rotate(-20deg)";
-        ball.style.color = "white";
-        h_boxes.style.visibility = "visible";
-        // h_boxes.style.height = "500px";
-
-    });
-
-    box.addEventListener("mouseleave", ()=>{
-
-        image.style.filter = "blur(0px)";
-        box.style.height = "950px";
-        box.style.width = "900px";
-        ball.style.width = "40px";
-        ball.style.height = "40px";
-        ball.style.borderRadius = "50%";
-        ball.style.transform = "translateX(0px)";
-        ball.style.color = "#4361ee";
-        h_boxes.style.visibility = "hidden";
-
-    });
-
+  box.addEventListener("mouseleave", () => {
+    image.style.filter = "blur(0px)";
+    box.style.height = "950px";
+    box.style.width = "900px";
+    ball.style.width = "40px";
+    ball.style.height = "40px";
+    ball.style.borderRadius = "50%";
+    ball.style.transform = "translateX(0px)";
+    ball.style.color = "#4361ee";
+    h_boxes.style.visibility = "hidden";
+  });
 });
 
 // const b1 = document.querySelector(".b1");
@@ -136,27 +174,30 @@ buttons.forEach((button, index) => {
 //     })
 // })
 
-const s_boxes = document.querySelectorAll(".sb1, .sb2, .sb3, .sb4, .sb5, .sb6, .sb7");
-const v_boxes = document.querySelectorAll(".sbh1, .sbh2, .sbh3, .sbh4, .sbh5, .sbh6, .sbh7");
+const s_boxes = document.querySelectorAll(
+  ".sb1, .sb2, .sb3, .sb4, .sb5, .sb6, .sb7",
+);
+const v_boxes = document.querySelectorAll(
+  ".sbh1, .sbh2, .sbh3, .sbh4, .sbh5, .sbh6, .sbh7",
+);
 const ser_p = document.querySelectorAll(".p1, .p2, .p3, .p4, .p5, .p6, .p7");
 
 s_boxes.forEach((s_box, index) => {
-    s_box.addEventListener("mouseenter", () => {
-        s_boxes.forEach((box) => {
-            if (box !== s_box) {
-                box.style.opacity = "0.3";
-                v_boxes[index].style.visibility = "visible";
-                ser_p[index].style.visibility = "visible";
-
-            }
-        });
+  s_box.addEventListener("mouseenter", () => {
+    s_boxes.forEach((box) => {
+      if (box !== s_box) {
+        box.style.opacity = "0.3";
+        v_boxes[index].style.visibility = "visible";
+        ser_p[index].style.visibility = "visible";
+      }
     });
+  });
 
-    s_box.addEventListener("mouseleave", () => {
-        s_boxes.forEach((box) => {
-            box.style.opacity = "1";
-            v_boxes[index].style.visibility = "hidden";
-            ser_p[index].style.visibility = "hidden";
-        });
+  s_box.addEventListener("mouseleave", () => {
+    s_boxes.forEach((box) => {
+      box.style.opacity = "1";
+      v_boxes[index].style.visibility = "hidden";
+      ser_p[index].style.visibility = "hidden";
     });
+  });
 });
